1c58e730de90480f0dd31f23616938bc2a3047f1
MikEAllEn338 <bartolomyuallen004@gmail.com>
2021-11-17 19:04:22 +0300

    currency functions

--- bot.js
+++ bot.js
@@ -1,31 +1,59 @@
 require("dotenv").config()
 
 const TelegramBot = require('node-telegram-bot-api');
+const { getUsdFromRub, getRubFromUsd, fromCurrency } = require("./lib/currency");
 
 // replace the value below with the Telegram token you receive from @BotFather
 const token = process.env.TOKEN;
 
 // Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot(token, { polling: true });
 
-// Matches "/echo [whatever]"
 bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
 
   const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
+  const resp = match[1];
 
-  // send back the matched "whatever" to the chat
   bot.sendMessage(chatId, resp);
 });
 
-// Listen for any kind of message. There are different kinds of
-// messages.
+bot.onText(/\/rub (.+)/, (msg, match) => {
+  const count = getUsdFromRub(match[1])
+  bot.sendMessage(msg.chat.id, count)
+})
+
+bot.onText(/\/usd (.+)/, (msg, match) => {
+  try {
+    const count = fromCurrency(match[1],"USD")
+    bot.sendMessage(msg.chat.id, count)
+
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+bot.onText(/\/from (.+)/, (msg, match) => {
+  try {
+    const [currency, count] = match[1].split(" ")
+    const res = fromCurrency(count, currency)
+    bot.sendMessage(msg.chat.id, res)
+  } catch (error) {
+    console.log(error)
+  }
+})
+
+bot.onText(/\/eur (.+)/, (msg, match) => {
+  try {
+    const count = fromCurrency(match[1],"EUR")
+    bot.sendMessage(msg.chat.id, count)
+
+  } catch (error) {
+    console.log(error)  
+  }
+})
+
 bot.on('message', (msg) => {
   const chatId = msg.chat.id;
 
-  // send a message to the chat acknowledging receipt of their message
   bot.sendMessage(chatId, 'Got it');
 });
